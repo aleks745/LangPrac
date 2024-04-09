7ece59dee675533ebcbf9443df899b550dd4c14a
aleks745 <86875268+aleks745@users.noreply.github.com>
2024-04-09 19:30:30 +0300

    Removed LanguageType and LanguageLvl fields from Language.cs class

--- Data/Language.cs
+++ Data/Language.cs
@@ -4,8 +4,6 @@
     {
         public int LanguageId { get; set; }
         public string? LanguageName { get; set; }
-        public string? LanguageType { get; set; }
-        public string? LanguageLvl { get; set; }
         public ICollection<UserLanguage>? UserLanguages { get; set; }
     }
 }
